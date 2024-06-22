8830a7c20371a7dd9dc4d748bb507804a72d0ea6
vi-nguyen <hoangvidue@gmail.com>
2024-06-22 11:58:23 -0400

    update lesson-02

---
+++ lessons/day-01/Variable.js
@@ -0,0 +1,27 @@
+// Declare
+let name = "An";
+console.log("Name: " + name);
+
+// Re-assign
+name = "Lan";
+console.log("New name: " + name);
+
+name = 18;
+console.log("New name: " + name)
+
+// Constant
+const PI_NUMBER = 3.14;
+console.log("PI number: " + PI_NUMBER)
+
+// Min value - Max value
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE)
+
+// Check the type of the value
+console.log(typeof(1.7976931348623157e+308))
+
+// Boolean
+let amIRight = false;
+amIRight = true
+console.log(typeof(amIRight))
+
