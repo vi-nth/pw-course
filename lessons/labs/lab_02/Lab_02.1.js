56550d3d79b8197bd4c77cbb60a38b8987732e34
vi-nguyen <hoangvidue@gmail.com>
2024-06-25 23:08:35 -0400

    Update Lab_02 final

--- lessons/labs/lab_02/Lab_02.1.js
+++ lessons/labs/lab_02/Lab_02.1.js
@@ -25,7 +25,7 @@ if (BMI < 18.5) {
  */
 if (BMI < 24.9) {
     let status = BMI < 18.5 ? "Underweight" : "Normalweight";
-    console.log("Status: " + status)
+    console.log("Status: " + status);
 } else if (BMI >= 25 && BMI <= 29.9) {
     console.log('Status: Overweight');
 } else {
--- lessons/labs/lab_02/Lab_02.3.js
+++ lessons/labs/lab_02/Lab_02.3.js
@@ -11,7 +11,7 @@ console.log(`BMI: ${BMI}`);
 if (BMI < 24.9) {
     let status = BMI < 18.5 ? "Underweight" : "Normalweight";
     let message = BMI < 18.5 ? "You should crease weight at least: " + (18.5 - BMI) : "Your weight is in the good range";
-    console.log("Status: " + status + "\n" +"Message: "+ message )
+    console.log("Status: " + status + "\n" +"Message: "+ message );
 } else  {
     let status = BMI >= 30 ? "Obesity" : "Overweight";
     console.log("Status: " + status);
